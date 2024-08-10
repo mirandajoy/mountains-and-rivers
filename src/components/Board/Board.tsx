26b44fbd316de56f002914445e007a64e0b8bac0
Miranda Neerhof <miranda.neerhof@gmail.com>
2024-08-10 17:38:52 -0300

    replace snake lines, ladder lines and tokens dots with basic visuals

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,12 +1,20 @@
 import "./Board.scss";
 import { ladderDetails } from "../../utils/calcLadderPos.ts";
 import { snakeDetails } from "../../utils/calcSnakePos.ts";
+import river from "../../assets/simple-river.svg";
+import riverS from "../../assets/simple-river-s.svg";
+import riverM from "../../assets/simple-river-m.svg";
+import riverL from "../../assets/simple-river-l.svg";
+import mountainS from "../../assets/simple-mountain-s.svg";
+import mountainM from "../../assets/simple-mountain-m.svg";
+import mountainL from "../../assets/simple-mountain-l.svg";
+import mountain from "../../assets/simple-mountain-2.svg";
 
 interface PlayerPosition {
   [key: number]: number;
 }
 
-function Board({ players, boardPosition }: { players: string[]; boardPosition: PlayerPosition }) {
+function Board({ boardPosition }: { boardPosition: PlayerPosition }) {
   const boardOrder: number[] = [
     100, 99, 98, 97, 96, 95, 94, 93, 92, 91, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 80, 79, 78, 77, 76, 75, 74, 73, 72,
     71, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 60, 59, 58, 57, 56, 55, 54, 53, 52, 51, 41, 42, 43, 44, 45, 46, 47, 48,
@@ -24,15 +32,33 @@ function Board({ players, boardPosition }: { players: string[]; boardPosition: P
             className={`board__space ${sq % 2 ? "board__space--odd" : "board__space--even"}`}
           >
             <div className="board__number">{sq}</div>
-            {boardPosition[0] == sq && <div className="board__token">{players[0]}</div>}
-            {boardPosition[1] == sq && <div className="board__token">{players[1]}</div>}
+            {boardPosition[0] == sq && (
+              <div className="board__token-container">
+                <span className="material-symbols-outlined board__token">eco</span>
+              </div>
+            )}
+            {boardPosition[1] == sq && (
+              <div className="board__token-container">
+                <span className="material-symbols-outlined board__token">spa</span>
+              </div>
+            )}
           </div>
         );
       })}
       {ladderDetails.map((l, i) => {
         const transformStyle = `rotate(-${l.angle}rad)`;
+        const img = () => {
+          if (l.hypot < 12) {
+            return mountainS;
+          } else if (l.hypot < 25) {
+            return mountainM;
+          } else {
+            return mountainL;
+          }
+        };
         return (
-          <div
+          <img
+            src={img()}
             key={i}
             className="board__ladder"
             style={{
@@ -41,13 +67,24 @@ function Board({ players, boardPosition }: { players: string[]; boardPosition: P
               width: `calc(${l.hypot}rem)`,
               transform: transformStyle,
             }}
-          ></div>
+          />
         );
       })}
       {snakeDetails.map((s, i) => {
+        console.log(s.hypot);
         const transformStyle = `rotate(-${s.angle}rad)`;
+        const img = () => {
+          if (s.hypot < 12) {
+            return riverS;
+          } else if (s.hypot < 25) {
+            return riverM;
+          } else {
+            return riverL;
+          }
+        };
         return (
-          <div
+          <img
+            src={img()}
             key={i}
             className="board__snake"
             style={{
@@ -56,7 +93,7 @@ function Board({ players, boardPosition }: { players: string[]; boardPosition: P
               width: `calc(${s.hypot}rem)`,
               transform: transformStyle,
             }}
-          ></div>
+          />
         );
       })}
     </div>
