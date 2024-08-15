5fa45e3b10398c668751d70a12e10b73db1468ba
Miranda Neerhof <miranda.neerhof@gmail.com>
2024-08-15 15:21:32 -0300

    add env variable for server host

--- src/context/SocketsContext.tsx
+++ src/context/SocketsContext.tsx
@@ -1,7 +1,7 @@
 import { createContext, useContext } from "react";
 import io from "socket.io-client";
 
-export const socket = io("http://localhost:8080");
+export const socket = io(import.meta.env.VITE_SERVER_HOST);
 
 export const SocketsContext = createContext<any>({ socket: null });
 
